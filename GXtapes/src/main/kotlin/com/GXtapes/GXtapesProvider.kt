a9633faadb8df95b9bfedb88984fe8a41f749ee0
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-12 06:41:04 +0700

    Update and rename FullboysProvider.kt to GXtapesProvider.kt

---
+++ GXtapes/src/main/kotlin/com/GXtapes/GXtapesProvider.kt
@@ -0,0 +1,14 @@
+package com.GXtapes
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class GXtapesProvider: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(GXtapes())
+        registerExtractorAPI(Stream())
+        registerExtractorAPI(VID())
+    }
+}
