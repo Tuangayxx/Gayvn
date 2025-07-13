47d44de7f5b1bad068e4366dd4cff48da0a35633
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-13 17:17:40 +0700

    done

--- Gayxx/src/main/kotlin/com/Gayxx/Extractor.kt
+++ Gayxx/src/main/kotlin/com/Gayxx/Extractor.kt
@@ -9,7 +9,6 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.newExtractorLink
-import com.lagradost.cloudstream3.utils.StreamTape
 import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.getQualityFromName
