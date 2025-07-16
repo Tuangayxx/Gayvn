1dd24a9b6d5186a723ffcf2cbf06803ff7c5539b
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-16 09:16:22 +0700

    ok

--- GPornOne/src/main/kotlin/com/Fullboys/GPornOneProvider.kt
+++ GPornOne/src/main/kotlin/com/Fullboys/GPornOneProvider.kt
@@ -1,12 +1,12 @@
-package com.Fullboys
+package com.GPornOne
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class FullboysProvider : Plugin() {
+class GPornOneProvider : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(Fullboys())
+        registerMainAPI(GPornOne())
     }
 }
