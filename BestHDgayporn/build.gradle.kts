651ecfd04e7833abdf508d69e7152c29dc7b21ae
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-16 12:46:18 +0700

    ok

--- BestHDgayporn/build.gradle.kts
+++ BestHDgayporn/build.gradle.kts
@@ -1,9 +1,9 @@
-version = 2
+version = 1
 
 cloudstream {
     authors     = listOf("Gayvn")
     language    = "en"
-    description = "Xtapes is the best free full length porn video site. Choose from millions of hardcore videos that stream quickly and in high quality and only full length"
+    description = "BestHDgayporn is the best free full length porn video site. Choose from millions of hardcore videos that stream quickly and in high quality and only full length"
 
     /**
      * Status int as the following:
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
-    iconUrl = "https://gay.xtapes.in/wp-content/uploads/logo6.png"
+    iconUrl = "https://besthdgayporn.com/wp-content/uploads/2023/11/cropped-New-Logo-Site-110423-32x32.png"
 }
