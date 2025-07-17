00dca91e7454b6ce58e60cbcc00da5a40843f342
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-18 05:18:19 +0700

    GPorntrex

--- GPorntrex/src/main/kotlin/com/GPorntrex/GEpornerProvider.kt
+++ GPorntrex/src/main/kotlin/com/GPorntrex/GEpornerProvider.kt
@@ -1,12 +1,12 @@
-package com.GEporner
+package com.GPorntrex
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class GEpornerProvider : Plugin() {
+class GPorntrexProvider : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(GEporner())
+        registerMainAPI(GPorntrex())
     }
 }
