c3dbc45e5f467aef953b7f52d3cd1a350d0c93e9
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-18 06:36:21 +0700

    Icegay

--- Icegay/src/main/kotlin/com/Icegay/IcegayProvider.kt
+++ Icegay/src/main/kotlin/com/Icegay/IcegayProvider.kt
@@ -1,12 +1,12 @@
-package com.GPorntrex
+package com.Icegay
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class GPorntrexProvider : Plugin() {
+class IcegayProvider : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(GPorntrex())
+        registerMainAPI(Icegay())
     }
 }
