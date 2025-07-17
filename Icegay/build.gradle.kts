b16149ca477c8602a5161142724314f92f37d64e
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-18 06:35:05 +0700

    ice

---
+++ Icegay/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("Gayvn")
+    language    = "en"
+    description = "Icegay is the best free full length porn video site. Choose from millions of hardcore videos that stream quickly and in high quality and only full length"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.icegay.tv/static/images/favicon/apple-icon-57x57-cb2ec1ec84.png"
+}
