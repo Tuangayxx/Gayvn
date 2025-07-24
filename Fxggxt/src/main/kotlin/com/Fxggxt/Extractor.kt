c954b5b9b06f9e86a332689da12691bdf0ef5611
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-24 20:06:11 +0700

    s

---
+++ Fxggxt/src/main/kotlin/com/Fxggxt/Extractor.kt
@@ -0,0 +1,39 @@
+package com.Fxggxt
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.api.Log
+import com.lagradost.cloudstream3*
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils*
+import org.json.JSONObject
+
+
+class DoodExtractor(
+    override val name: String = "doodstream",
+    override val mainUrl: String = "https://vide0.net/e/",
+    override val requiresReferer: Boolean = false
+) : ExtractorApi() {
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val document = app.get(url).document
+        var found = false
+
+        document.select("div.responsive-player iframe").forEach { iframe ->
+            val src = iframe.attr("src")
+            val videoHash = src.substringAfter("/")
+            val directUrl = "$mainUrl/$videoHash"
+        callback(
+                newExtractorLink(
+                    this.name,
+                    this.name,
+                    directUrl,
+                    ExtractorLinkType.M3U8
+                )
+            )
+        }
+    }
+}
