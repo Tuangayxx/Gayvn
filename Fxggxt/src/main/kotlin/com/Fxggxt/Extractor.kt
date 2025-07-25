41626283a862405168d57181a4c14f7f24b6134a
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-26 02:11:09 +0700

    Update Extractor.kt

--- Fxggxt/src/main/kotlin/com/Fxggxt/Extractor.kt
+++ Fxggxt/src/main/kotlin/com/Fxggxt/Extractor.kt
@@ -7,7 +7,6 @@ import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.*
 import org.json.JSONObject
 
-
 class DoodExtractor(
     override val name: String = "doodstream",
     override val mainUrl: String = "https://vide0.net/e/",
@@ -19,14 +18,22 @@ class DoodExtractor(
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val document = app.get(url).document
+        // Chuyển đổi URL từ doodstream.com sang vide0.net
+        val convertedUrl = url.replace(
+            oldValue = "doodstream.com",
+            newValue = "vide0.net",
+            ignoreCase = true
+        )
+        
+        val document = app.get(convertedUrl).document
         var found = false
 
         document.select("div.responsive-player iframe").forEach { iframe ->
             val src = iframe.attr("src")
             val videoHash = src.substringAfter("/")
-            val directUrl = "$mainUrl/$videoHash"
-        callback(
+            val directUrl = "$mainUrl$videoHash"
+            
+            callback(
                 newExtractorLink(
                     this.name,
                     this.name,
@@ -34,6 +41,11 @@ class DoodExtractor(
                     ExtractorLinkType.M3U8
                 )
             )
+            found = true
+        }
+        
+        if (!found) {
+            Log.warn("Không tìm thấy iframe phát video trong trang")
         }
     }
 }
