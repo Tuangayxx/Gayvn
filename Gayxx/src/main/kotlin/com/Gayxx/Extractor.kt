b0e2871d18ca01f845fdd8b12a0289320d65655b
Tuangayxx <gayboyfindfun@gmail.com>
2025-07-25 15:07:14 +0700

    a

--- Gayxx/src/main/kotlin/com/Gayxx/Extractor.kt
+++ Gayxx/src/main/kotlin/com/Gayxx/Extractor.kt
@@ -12,7 +12,6 @@ import com.lagradost.api.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.USER_AGENT
 import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.utils
 import org.jsoup.nodes.Element
 import org.jsoup.nodes.Document
 import kotlinx.coroutines.delay
