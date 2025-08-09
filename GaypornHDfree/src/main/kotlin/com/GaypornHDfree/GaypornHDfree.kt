59079cc4108577c9623e622735dc18d6df958515
Tuangayxx <gayboyfindfun@gmail.com>
2025-08-10 00:03:26 +0700

    Update GaypornHDfree.kt

--- GaypornHDfree/src/main/kotlin/com/GaypornHDfree/GaypornHDfree.kt
+++ GaypornHDfree/src/main/kotlin/com/GaypornHDfree/GaypornHDfree.kt
@@ -21,6 +21,11 @@ class GaypornHDfree : MainAPI() {
     override val hasChromecastSupport = true
     override val supportedTypes       = setOf(TvType.NSFW)
     override val vpnStatus            = VPNStatus.MightBeNeeded
+
+    override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
+    override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
+    override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
+	*/
         
     override val mainPage = mainPageOf(
             ""                              to "Mới cập nhật",
