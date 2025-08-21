b3e89d5f0a6f285f217dd66a54fd18d56b2c4b63
Tuangayxx <gayboyfindfun@gmail.com>
2025-08-21 11:38:39 +0700

    d

--- HDgay/build.gradle.kts
+++ HDgay/build.gradle.kts
@@ -1,9 +1,9 @@
-version = 2
+version = 1
 
 cloudstream {
     authors     = listOf("Gayvn")
-    language    = "en"
-    description = "Gayxx.net is the best free Asian porn video site."
+    language    = "vi"
+    description = "HDgay is the best free Asian porn video site."
 
     /**
      * Status int as the following:
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 0 // will be 3 if unspecified
+    status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
-    iconUrl = "https://gayxx.net/wp-content/themes/yolotube-mod-v2/images/logo.png"
+    iconUrl = "https://hdgay.net/wp-content/themes/yolotube-mod-v2/images/logo.png"
 }
