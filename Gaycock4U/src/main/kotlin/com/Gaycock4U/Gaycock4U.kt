79ac1cb6e94e0c5e186b23dfd699307dfa2b372a
Tuangayxx <gayboyfindfun@gmail.com>
2025-08-27 23:06:38 +0700

    v

--- Gaycock4U/src/main/kotlin/com/Gaycock4U/Gaycock4U.kt
+++ Gaycock4U/src/main/kotlin/com/Gaycock4U/Gaycock4U.kt
@@ -48,7 +48,7 @@ class Gaycock4U : MainAPI() {
             "${request.data}"
         }
 
-        val ua = mapOf("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+        val ua = mapOf("User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
         val document = app.get(url, headers = ua).document
         // Fixed selector - using correct container class
         val home = document.select("div.elementor-widget-container article.elementor-post").mapNotNull { it.toSearchResult() }
