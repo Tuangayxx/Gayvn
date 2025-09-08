21aced941fed9adf9651fe68fa825dd847a0beda
Tuangayxx <gayboyfindfun@gmail.com>
2025-09-08 20:26:49 +0700

    ib

---
+++ iGay69/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 2 
+
+cloudstream {
+    authors     = listOf("Gayvn")
+    language    = "en"
+    description = "iGay69 is the best free porn video site."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://cdn.igay69.com/wp-content/uploads/2023/10/06172235/Logo-2.png"
+}
---
+++ iGay69/src/main/kotlin/com/iGay69/iGay69Provider.kt
@@ -0,0 +1,21 @@
+package com.iGay69
+
+import com.lagradost.cloudstream3.extractors.*
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.iGay69.iGay69
+
+@CloudstreamPlugin
+class iGay69Provider: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(iGay69())
+        registerExtractorAPI(Bigwarp())
+        registerExtractorAPI(Voe())
+        registerExtractorAPI(VoeExtractor())
+        registerExtractorAPI(dsio())
+        registerExtractorAPI(DoodstreamCom())
+        registerExtractorAPI(vide0())
+        registerExtractorAPI(ListMirror())
+    }
+}
