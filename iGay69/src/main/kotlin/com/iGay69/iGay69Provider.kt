ac90e43789a49ac5c2b5feb7da876b7c38476a8b
Tuangayxx <gayboyfindfun@gmail.com>
2025-09-09 16:41:08 +0700

    ag

--- iGay69/src/main/kotlin/com/iGay69/iGay69Provider.kt
+++ iGay69/src/main/kotlin/com/iGay69/iGay69Provider.kt
@@ -3,12 +3,13 @@ package com.iGay69
 import com.lagradost.cloudstream3.extractors.*
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.*
 import android.content.Context
 import com.iGay69.iGay69
 
 @CloudstreamPlugin
-class iGay69Provider: Plugin() {
-    override fun load(context: Context) {
+class iGay69Provider: BasePlugin() {
+    override fun load() {
         registerMainAPI(iGay69())
         registerExtractorAPI(Bigwarp())
         registerExtractorAPI(Voe())
